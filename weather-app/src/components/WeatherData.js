4c597a31eb58860ae05dc558414b659c09827a48
DanielGeek <daniel30081990@gmail.com>
2020-05-30 18:36:03 -0400

    Utilización de constantes

--- weather-app/src/components/WeatherData.js
+++ weather-app/src/components/WeatherData.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from './../constants/weather';
 
 const WeatherData = () => (
     <div>
         <WeatherTemperature 
             temperature={20}
-            weatherState={''} 
+            weatherState={CLOUDY} 
         />
         <WeatherExtraInfo humidity={80} wind={"10 m/s"} />
     </div>
